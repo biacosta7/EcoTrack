0ee45ebf1f7c9d412fda5e64e4967048e86ebe9d
biacosta7 <contact.beatrizcosta@gmail.com>
2024-09-29 16:58:30 -0300

    Organização das pastas

--- agendamentos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'agendamentos'
-
-urlpatterns = [
-    path('', views.agendar, name='agendamentos_coleta'),  # View para o formulário de agendamento
-    path('confirmacao/', views.confirmacao_view, name='confirmacao'),  # View de confirmação
-]
